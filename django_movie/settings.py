e1bbace0519496e3829e3f8a2b6e4af9508e9b86
Julia Kaushan <julia-kaushan@mail.ru>
2020-10-25 16:56:10 +0300

    Секретный ключ из env

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u0qyo)_t5b2r1xg)m=2j5f27=p=at9(_isv_!cmm*y4)!!vi93'
+SECRET_KEY = os.environ.get('SEKRET_KEY_DJANGO_MOVIES')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
