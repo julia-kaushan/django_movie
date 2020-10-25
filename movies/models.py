54e463270195a43c699ec82751d17044137ea3a0
Julia Kaushan <julia-kaushan@mail.ru>
2020-10-25 17:49:32 +0300

    Создание класса "Жанры"

--- movies/models.py
+++ movies/models.py
@@ -28,3 +28,16 @@ class Actor(models.Model):
         verbose_name = "Актеры и режиссеры"
         verbose_name_plural = "Актеры и режиссеры"
 
+
+class Genre(models.Model):
+    """Жанры"""
+    name = models.CharField("Имя", max_length=100)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
