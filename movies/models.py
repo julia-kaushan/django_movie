a23eeb46da2aea499e68fb3a363d1888b08be927
Julia Kaushan <julia-kaushan@mail.ru>
2020-10-25 17:47:33 +0300

    Создание класса "Актеры и режиссеры"

--- movies/models.py
+++ movies/models.py
@@ -14,3 +14,17 @@ class Category(models.Model):
         verbose_name_plural = "Категории"
 
 
+class Actor(models.Model):
+    """Актеры и режиссеры"""
+    name = models.CharField("Имя", max_length=100)
+    age = models.PositiveSmallIntegerField("Возраст", default=0)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="actors/")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Актеры и режиссеры"
+        verbose_name_plural = "Актеры и режиссеры"
+
