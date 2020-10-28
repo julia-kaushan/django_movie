f63d40ab5bce5eb1846dbae07982937fdde6c971
Julia Kaushan <julia-kaushan@mail.ru>
2020-10-28 20:34:05 +0300

    Создание представления для класса "MovieDetailView" и корректировка маршрутизации

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from datetime import date
 
+from django.urls import reverse
+
+
 class Category(models.Model):
     """Категории"""
     name = models.CharField("Категория", max_length=150)
@@ -65,6 +68,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
 
     class Meta:
         verbose_name = "Фильм"
