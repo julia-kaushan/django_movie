0fb2017ae2254b42c9a902b648e593cd95baba9f
Julia Kaushan <julia-kaushan@mail.ru>
2020-11-06 15:41:24 +0300

    Вывод кадров из фильма и формы отзывов

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
@@ -16,3 +16,10 @@ class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        print(request.POST)
+        return redirect("/")
