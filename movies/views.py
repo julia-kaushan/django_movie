97e0a0dfe3375a209c391172e7e07934af1f2c97
Julia Kaushan <julia-kaushan@mail.ru>
2020-11-06 16:14:50 +0300

    Реализация отправки отзыва от клиента и сохранение в БД

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
-
+from .forms import ReviewForm
 
 class MoviesView(ListView):
     """Список фильмов"""
@@ -21,5 +21,10 @@ class MovieDetailView(DetailView):
 class AddReview(View):
     """Отзывы"""
     def post(self, request, pk):
-        print(request.POST)
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
         return redirect("/")
