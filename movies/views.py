7a8c020d048021344216517f83ff58c309c982c5
Julia Kaushan <julia-kaushan@mail.ru>
2020-11-22 14:59:21 +0300

    Создание пагинации к фильтрам на главной странице с фильмами

--- movies/views.py
+++ movies/views.py
@@ -21,7 +21,7 @@ class MoviesView(GenreYear, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    paginate_by = 1
+    paginate_by = 3
 
 
 class MovieDetailView(GenreYear, DetailView):
@@ -58,13 +58,21 @@ class ActorView(GenreYear, DetailView):
 
 class FilterMoviesView(GenreYear, ListView):
     """Фильтр фильмов"""
+    paginate_by = 2
+
+
     def get_queryset(self):
         queryset = Movie.objects.filter(
             Q(year__in=self.request.GET.getlist("year")) |
             Q(genres__in=self.request.GET.getlist("genre"))
-        )
+        ).distinct()
         return queryset
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["year"] = ''.join([f"year={x}&" for x in self.request.GET.getlist(year)])
+        context["genre"] = ''.join([f"genre={x}&" for x in self.request.GET.getlist(genre)])
+        return context
 
 class JsonFilterMoviesView(ListView):
     """Фильтр фильмов в json"""
